dd1e32cf166f0d2b4836aa43d1c2d692ad9540c5
vanTHkrab <vanth2548@gmail.com>
2025-06-02 17:18:41 +0700

    update test get hello page form nest

--- my-vanthkrab-nest/src/app.controller.ts
+++ my-vanthkrab-nest/src/app.controller.ts
@@ -7,17 +7,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(@Headers('x-api-key') xApiKey: string): string {
-    try {
-      console.log('x-api-key:', xApiKey);
-      if (!xApiKey) {
-        throw new Error('x-api-key header is missing');
-      }
-      return this.appService.getHello();
-    }
-    catch (error) {
-      console.error('Error:', error.message);
-      return `Error: ${error.message}`;
-    }
+  getHello() {
+    return this.appService.getHello();
   }
 }
