52ecda6560aa068abd6d84673ea456df5bd9c928
vanTHkrab <vanth2548@gmail.com>
2026-04-15 18:21:44 +0700

    -rm package not used. -rm file auth, prisma, and other

--- my-vanthkrab-web/prisma/migrations/20250519100839_add_table_message/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Message" (
-    "id" SERIAL NOT NULL,
-    "sender" TEXT NOT NULL,
-    "content" TEXT NOT NULL,
-
-    CONSTRAINT "Message_pkey" PRIMARY KEY ("id")
-);
